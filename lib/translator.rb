7ca665febe9fb4d97073282fdaf03081d11f5fe8
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 09:08:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 def load_library(file_path)
-  array = yaml.load_library('./lib/emoticons.yml')
+  my_hash = yaml.load_file('./lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
